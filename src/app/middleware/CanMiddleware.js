fd4a353bcb42b7ea4fa1c98418bcbf585f59556b
MarcosVBarbosa <marcosvbarbosadev@outlook.com>
2026-04-22 19:51:07 -0300

    --

--- src/app/middleware/CanMiddleware.js
+++ src/app/middleware/CanMiddleware.js
@@ -1,14 +1,23 @@
 // middleware/CanMiddleware.js
-export function can(resource, action) {
-  return (req, res, next) => {
-    const crud = req.user?.roles?.crud;
 
-    if (!crud) {
+export function can(resource, action = 'view') {
+  return (req, res, next) => {
+    const permissions = req.permissions;
+
+    if (!permissions) {
+      return res.status(403).json({ error: 'Sem permissão' });
+    }
+
+    const module = permissions[resource?.toLowerCase()];
+
+    if (!module) {
       // return res.status(403).json({ error: 'Sem permissão' });
     }
 
-    if (!crud[resource]?.includes(action)) {
-      // return res.status(403).json({ error: 'Sem permissão' });
+    const allowed = module[action];
+
+    if (!allowed) {
+      // return res.status(403).json({ error: 'Acesso negado' });
     }
 
     return next();
